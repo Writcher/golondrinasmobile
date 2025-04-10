adf8dd3d6aa0f22f655130b5a34064b5eca8b6fa
Joaquín Giménez <joaquingim77@gmail.com>
2025-04-10 18:43:24 -0300

    ins and outs service

--- lib/types/reservations.ts
+++ lib/types/reservations.ts
@@ -2,3 +2,11 @@ export type fetchReservationListData = {
     month: number,
     year: number,
 };
+
+export type fetchTodayInsListData = {
+    dateIn: Date,
+};
+
+export type fetchTodayOutsListData = {
+    dateOut: Date,
+};
--- services/reservations.ts
+++ services/reservations.ts
@@ -1,4 +1,4 @@
-import { fetchReservationListData } from "@/lib/types/reservations";
+import { fetchReservationListData, fetchTodayInsListData, fetchTodayOutsListData } from "@/lib/types/reservations";
 
 export async function fetchReservationList(data: fetchReservationListData) {
     try {
@@ -29,4 +29,64 @@ export async function fetchReservationList(data: fetchReservationListData) {
         console.error('Error fetching reservation list:', error);
         throw error;
     };
-}
+};
+
+export async function fetchTodayInsList(data: fetchTodayInsListData) {
+    try {
+        const API_URL = process.env.EXPO_PUBLIC_API_URL;
+        const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
+
+        const queryParams = new URLSearchParams({
+            dateIn: data.dateIn.toISOString(),
+        });
+
+        const response = await fetch(`${API_URL}reservations?${queryParams.toString()}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${API_KEY}`,
+            },
+        });
+
+        if (!response.ok) {
+            const errorResponse = await response.json();
+            throw new Error(errorResponse.error || 'An error occurred while fetching the reservations list');
+        };
+
+        const responseData = await response.json();
+        return responseData;
+
+    } catch (error) {
+        console.error('Error fetching reservation list:', error);
+        throw error;
+    };
+};
+
+export async function fetchTodayOutsList(data: fetchTodayOutsListData) {
+    try {
+        const API_URL = process.env.EXPO_PUBLIC_API_URL;
+        const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
+
+        const queryParams = new URLSearchParams({
+            dateIn: data.dateOut.toISOString(),
+        });
+
+        const response = await fetch(`${API_URL}reservations?${queryParams.toString()}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${API_KEY}`,
+            },
+        });
+
+        if (!response.ok) {
+            const errorResponse = await response.json();
+            throw new Error(errorResponse.error || 'An error occurred while fetching the reservations list');
+        };
+
+        const responseData = await response.json();
+        return responseData;
+
+    } catch (error) {
+        console.error('Error fetching reservation list:', error);
+        throw error;
+    };
+};
